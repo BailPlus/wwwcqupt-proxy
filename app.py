5bb83e3023a92b359bf4f2c1fa456a92c0df88e4
Bail <2915289604@qq.com>
2025-01-08 12:36:51 +0800

    增加了拉黑和请求日志功能

--- app.py
+++ app.py
@@ -4,23 +4,34 @@
 
 TARGET = 'http://localhost:5000'
 
-from flask import Flask,request,make_response
+from flask import Flask,request,make_response,abort
 import httpx,sys
 
-blacklist = []
+ip_blacklist = []   # ip黑名单，用于过滤频繁访问
 
 class Proxy(Flask):
     def __init__(self):
         super().__init__(__name__)
         self.before_request(self._before_request)
     def _before_request(self):
+        # 过滤黑名单ip
+        if request.remote_addr in ip_blacklist:
+            abort(403,'检测到你有违规操作，已禁止访问。如有疑问，请咨询Bail。')
+        # 打印请求
+        with open('traffic.log', 'ab') as requests_log_file:
+            requests_log_file.write(
+                b'-' * 5 + request.remote_addr.encode() + b' ' + request.method.encode() + b' ' + request.full_path.encode() + b'\n')
+            requests_log_file.write(str(request.headers).encode() + b'\n\n')
+            requests_log_file.write(request.get_data() + b'\n')
+        # 处理请求
         req_headers = request.headers.to_wsgi_list()
         req_headers.append(('X-Real-IP',request.remote_addr))
         resp = httpx.request(request.method,TARGET+request.full_path,headers=req_headers,data=request.get_data())
-        if resp.status_code == 600:
-            print('600 status code received!')
         ready_resp = make_response(resp.content,f'{resp.status_code} {resp.reason_phrase}')
         ready_resp.headers = dict(resp.headers)
+        # 处理来自主服务器的拉黑请求
+        if resp.status_code == 601:
+            ip_blacklist.append(request.remote_addr)
         return ready_resp
 
 if __name__ == '__main__':
