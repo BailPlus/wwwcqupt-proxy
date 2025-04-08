3aba76956a312cfbe6d050c18cc5189bcaea3c26
Bail <2915289604@qq.com>
2025-04-08 22:45:11 +0800

    移除了黑名单缓存机制,实时查表

--- libblacklist.py
+++ libblacklist.py
@@ -4,13 +4,10 @@ import sqlite3
 
 class BlacklistHandler:
     '''黑名单处理类'''
-    _blacklist:set  # ip黑名单
     conn:sqlite3.Connection  # sqlite3连接
     def __init__(self,db_path:str=BLACKLIST_DB):
         self.conn = sqlite3.connect(db_path,check_same_thread=False) # XXX: 线程不安全
-        self._blacklist = set()
         self.create_table()
-        self.load_blacklist()
     def __del__(self):
         '''析构函数'''
         self.conn.close()
@@ -19,26 +16,26 @@ class BlacklistHandler:
         c = self.conn.cursor()
         c.execute('CREATE TABLE IF NOT EXISTS blacklist (ip VARCHAR(16) PRIMARY KEY)')
         self.conn.commit()
-    def load_blacklist(self):
+    def get_all(self):
         '''加载黑名单'''
         c = self.conn.cursor()
+        blacklist = set()
         for row in c.execute('SELECT ip FROM blacklist'):
-            self._blacklist.add(row[0])
-        return self._blacklist
+            blacklist.add(row[0])
+        return blacklist
     def add(self,ip:str):
         '''添加ip到黑名单'''
-        if ip not in self._blacklist:
-            c = self.conn.cursor()
-            self._blacklist.add(ip)
-            c.execute('INSERT INTO blacklist (ip) VALUES (?)',(ip,))
-            self.conn.commit()
+        c = self.conn.cursor()
+        c.execute('''INSERT INTO blacklist (ip) SELECT ?
+                     WHERE NOT EXISTS (
+                       SELECT 1 FROM blacklist WHERE ip = ?
+                     )''',(ip,ip))
+        self.conn.commit()
     def is_in_blacklist(self,ip:str)->bool:
         '''判断ip是否在黑名单'''
-        return ip in self._blacklist
+        return self.conn.cursor().execute('SELECT 1 FROM blacklist WHERE ip=?',(ip,)).fetchone() is not None
     def remove_ip(self,ip:str):
         '''从黑名单中删除ip'''
-        if ip in self._blacklist:
-            c = self.conn.cursor()
-            self._blacklist.remove(ip)
-            c.execute('DELETE FROM blacklist WHERE ip=?',(ip,))
-            self.conn.commit()
+        c = self.conn.cursor()
+        c.execute('DELETE FROM blacklist WHERE ip=?',(ip,))
+        self.conn.commit()
